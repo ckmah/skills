980813897e348d02b56657d5ca140543e5f1f779
Clarence Mah <ckmah@users.noreply.github.com>
2026-05-31 12:12:30 -0700

    Fix Windows Task Scheduler repetition duration limit.

--- bin/scheduler/install-windows.ps1
+++ bin/scheduler/install-windows.ps1
@@ -6,7 +6,7 @@ node $cli sync --quiet
 $action = New-ScheduledTaskAction -Execute 'node' -Argument "`"$cli`" sync --quiet"
 $triggers = @(
   (New-ScheduledTaskTrigger -AtLogOn),
-  (New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 15) -RepetitionDuration ([TimeSpan]::MaxValue))
+  (New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 15) -RepetitionDuration (New-TimeSpan -Days 3650))
 )
 Register-ScheduledTask -TaskName 'CkmahSkillsSync' -Action $action -Trigger $triggers -Force | Out-Null
 Write-Output 'Registered CkmahSkillsSync task.'
